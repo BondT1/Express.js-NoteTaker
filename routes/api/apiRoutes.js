7725ec79082175520aea7ab2f342eccc325197b0
BondT1 <tombond248@gmail.com>
2022-12-14 18:44:47 +0000

    more work on html routes and server.js

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
-const { createNote, deleteNote } = require('../../lib/createNotes');
-let { array } = require('../../db/db.json');
+// const router = require('express').Router();
+// const { createNote, deleteNote } = require('../../lib/createNotes');
+// let { array } = require('../../db/db.json');
 
-router.get('/api/notes', (req, res) => {
-    let content = array;
-    res.json(content);
-});
+// router.get('/api/notes', (req, res) => {
+//     let content = array;
+//     res.json(content);
+// });
 
-router.post('/api/notes', (req, res) => {
-    if(array) {
-        req.body.id = array.length.toString();
-    } else {
-        req.body.id = 0}
-    res.json(create(req.body, array));
-});
+// router.post('/api/notes', (req, res) => {
+//     if(array) {
+//         req.body.id = array.length.toString();
+//     } else {
+//         req.body.id = 0}
+//     res.json(create(req.body, array));
+// });
 
-// create deleteNote function
+// // create deleteNote function
--- server.js
+++ server.js
@@ -1,21 +1,40 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
+// const api = require('./routes/api');
+// const html = require('./routes/html');
+
+// app.use('/api', api);
+// app.use('/', html);
+
+
+// app.listen(PORT, () => {
+//     console.log(`API Server situated on PORT: ${PORT}`)
+// })
+
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-const api = require('./routes/api');
-const html = require('./routes/html');
+const path = require('path');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', api);
-app.use('/', html);
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+// router.get('*', (req, res) => {
+//      res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
+
+app.get('/api/notes', (req, res) => {
+  res.json('notes')
+})
 
 app.listen(PORT, () => {
-    console.log(`API Server situated on PORT: ${PORT}`)
+  console.log(`API Server situated on PORT: ${PORT}`)
 })
