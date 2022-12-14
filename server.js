2500676987f55f061216b2f941c235f69a6376ba
BondT1 <tombond248@gmail.com>
2022-12-14 19:36:44 +0000

    additional server.js changes

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+const router = require('express').Router();
+let { array } = require('./db/db.json');
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -27,14 +29,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-// router.get('*', (req, res) => {
-//      res.sendFile(path.join(__dirname, '../../public/index.html'));
-// });
+// app.get('/api/notes', (req, res) => {
+//   res.json('notes')
+// })
 
 app.get('/api/notes', (req, res) => {
-  res.json('notes')
-})
+    let content = array;
+    res.json(content);
+});
 
 app.listen(PORT, () => {
   console.log(`API Server situated on PORT: ${PORT}`)
 })
+
+
+// app.post('/api/notes', (req, res) => {
+//     req.body.id = generateUniqueId();
+//     const note = createNote(req.body, notes);
+//     res.json(note);
+//   });
+
+// router.get('*', (req, res) => {
+//      res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
+
+// const { createNote, deleteNote } = require('./lib/createNotes');
