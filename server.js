f5d18e96ba6729fd0bd7c4d779a90e5a1fcb969f
BondT1 <tombond248@gmail.com>
2022-12-15 19:09:42 +0000

    minimal changes to server.js

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const app = express()
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 const router = require('express').Router();
-const data = require('./db/db.json')
+const data = require('./db/db.json');
+const uniqueId = require('generate-unique-id');
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function createNote(body, data) {
+    const note = body;
+    data.push(note);
+    fstat.writeFileSync(
+        path.join(__dirname, './db/db.json')
+    )
+    return note;
+}
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
@@ -21,6 +31,12 @@ app.get('/', (req, res) => {
 app.get('/api/notes', (req, res) => {
     let content = data;
     res.json(content);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uniqueId();
+    const noteCreate = createNote(req.body, data);
+    res.json(noteCreate)
 });
 
 app.listen(PORT, () => {
